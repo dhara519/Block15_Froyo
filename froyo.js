7974f0d32dc1551bee249ddaafbd58a605a35eab
dhara519 <dhara519@gmail.com>
2024-06-24 23:23:07 -0400

    complete

--- froyo.js
+++ froyo.js
@@ -1,10 +1,12 @@
 // prompt("Enter a list of comma separated froyo flavors");
-let chosenFlavors = prompt("Enter a list of comma separated froyo flavors");
+const chosenFlavors = prompt(
+  "Enter a list of comma separated froyo flavors"
+).split(",");
 // choosenFlavors = [vanilla,vanilla,vanilla,strawberry,coffee,coffee]
 
 function filterPush() {
   let table = {};
-  Array.from(chosenFlavors).forEach((flavor) => {
+  chosenFlavors.forEach((flavor) => {
     flavor in table ? (table[flavor] += 1) : (table[flavor] = 1);
   });
   return table;
